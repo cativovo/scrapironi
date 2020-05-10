a1ab2cdb2e33c23869bfba9cdc28dbee2bacafd9
cativovo <cativomarkleo@gmail.com>
2020-05-10 22:27:56 +0800

    magic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,67 @@
-console.log('it is working!');
+import axios from 'axios';
+import cors from 'cors';
+import express from 'express';
+import { load } from 'cheerio';
+
+type RankData = Record<'rank' | 'changeInRank' | 'category' | 'rankHistory', string>;
+type RanksInPlatforms = Record<'applePodcasts' | 'spotify', RankData[]>;
+
+const scrapeRanking = async (podcast: string): Promise<RanksInPlatforms> => {
+  const url = `https://chartable.com/podcasts/${podcast}/charts_partial`;
+
+  const res = await axios.get(url);
+  const $ = load(res.data);
+  const [, , applePodcastTable, , spotifyTable] = $('div').children().toArray();
+
+  const getRankings = (chartType: string) => (
+    ranksInPlatForm: RankData[],
+    trElement: CheerioElement,
+    index: number
+  ): RankData[] => {
+    if (index === 0) {
+      return ranksInPlatForm;
+    }
+    const [rank, changeInRank, category, rankHistory] = $(trElement).find('td').toArray();
+    const rankHistoryHref = $(rankHistory).find('a').attr('href') as string;
+
+    const rankData = {
+      rank: $(rank).text().trim(),
+      changeInRank: $(changeInRank).text().trim(),
+      category: $(category).text().trim(),
+      rankHistory: `https://chartable.com/api/podcasts/${podcast}/rank_history?chart_id=${rankHistoryHref.slice(
+        rankHistoryHref.lastIndexOf('/') + 1
+      )}&chart_type=${chartType}`,
+    };
+
+    return [...ranksInPlatForm, rankData];
+  };
+
+  const applePodcasts = $(applePodcastTable)
+    .find('tr')
+    .toArray()
+    .reduce<RankData[]>(getRankings('itunes'), []);
+
+  const spotify = $(spotifyTable)
+    .find('tr')
+    .toArray()
+    .reduce<RankData[]>(getRankings('spotify'), []);
+
+  return {
+    applePodcasts,
+    spotify,
+  };
+};
+
+const app = express();
+
+app.use(cors());
+
+app.get('/:podcast', async (req, res) => {
+  const { podcast } = req.params;
+  const ranksInPlatforms = await scrapeRanking(podcast);
+
+  res.send(ranksInPlatforms);
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
